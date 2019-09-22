dedf4281b740de8dc9b5e2e1f4497edad8a879f9
Evan Perret-Timms <evan.perret@ca-cal01-lt177.local>
2019-09-22 16:34:03 -0600

    added requests, cars and reviews table

--- assignmentOne/group-P2.sql
+++ assignmentOne/group-P2.sql
@@ -31,9 +31,47 @@ Requests: (NOT SURE YET)
     - p.k is location lcode + dst lcode + memember email
 */
 
+CREATE TABLE reviews(
+    rid INT NOT NULL,
+    rdate DATETIME,
+    rtext VARCHAR(1000),
+    rscore INT,
+    reviewer VARCHAR(30) NOT NULL,
+    reviewee VARCHAR(30) NOT NULL,
+    ride_number VARCHAR(30),
+    PRIMARY KEY(rid),
+    FOREIGN KEY (reviewer) REFERENCES members,
+    FOREIGN KEY (reviewee) REFERENCES members,
+    FOREIGN KEY (ride_number) REFERENCES rides
+)
+
+CREATE TABLE cars(
+    cno INT NOT NULL,
+    make VARCHAR(10),
+    model VARCHAR(15),
+    model_year DATETIME,
+    seats INT,
+    gdate DATETIME,
+    registered_driver VARCHAR(30) NOT NULL,
+    PRIMARY KEY (rno),
+    FOREIGN KEY (registered_driver) REFERENCES members
+)
+
 CREATE TABLE members(
     phone CHAR(9),
     name VARCHAR(30),
     email VARCHAR(30),
     PRIMARY KEY(email)
+)
+
+CREATE TABLE requests (
+    amount INT,
+    qdate DATETIME,
+    dst_code VARCHAR(30),
+    lc_code VARCHAR(30),
+    request_email VARCHAR(30) NOT NULL,
+    FOREIGN KEY(dst_code) REFERENCES locations,
+    FOREIGN KEY(lc_code) REFERENCES locations,
+    FOREIGN KEY(request_email) REFERENCES members,
+    PRIMARY KEY (dst_code, lc_code, request_email)
 )
