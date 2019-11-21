8cd4aaffb7c83a53772e13b9c07b6cba6bc2b6e6
Evan Perret-Timms <evan.perret@ca-cal01-lt177.local>
2019-11-21 14:38:06 -0700

    Wrote boilerplate function

--- MiniProjectTwo/PrepareFiles.py
+++ MiniProjectTwo/PrepareFiles.py
@@ -8,13 +8,36 @@ emails = open("emails.txt", "w")
 dates = open("dates.txt", "w")
 recs = open("recs.txt", "w")
 
-for l in xml:
-    if re.match('<mail>.*</mail>', l):
-        row = re.search("<row>(.*)</row>", l).group(1)
-        date = re.search("<date>(.*)</date>", l).group(1)
-        frm = re.search("<from>(.*)</from>", l).group(1)
-        to = re.search("<to>(.*)</to>", l).group(1)
-        body = re.search("<body>(.*)</body>", l).group(1)
+def writeToTerms(row, subject, body):
+    pass
 
-        print(row, date, frm, to, body)
+def writeToEmails(row, frm, to, cc, bcc):
+    pass
+
+def writeToRecs(row, date):
+    pass
+
+def writeToDates(row, line):
+    pass
+
+
+def main():
+    for l in xml:
+        if re.match('<mail>.*</mail>', l):
+            row = re.search("<row>(.*)</row>", l).group(1)
+            date = re.search("<date>(.*)</date>", l).group(1)
+            frm = re.search("<from>(.*)</from>", l).group(1)
+            to = re.search("<to>(.*)</to>", l).group(1)
+            bcc = re.search("<bcc>(.*)</bcc>", l).group(1)
+            cc = re.search("<cc>(.*)</cc>", l).group(1)
+            body = re.search("<body>(.*)</body>", l).group(1)
+            subj = re.search("<subj>(.*)</subj>", l).group(1)
+
+            writeToTerms(row, subj, body)
+            writeToEmails(row, frm, to, bcc, cc)
+            writeToDates(row, date)
+            writeToRecs(row, l)
         
+
+if __name__ == "__main__":
+    main()
