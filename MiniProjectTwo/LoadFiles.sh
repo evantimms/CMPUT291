2cd649518c4f7bc108d2cdc95777fe5a0007f510
Evan Perret-Timms <evan.perret@ca-cal01-lt177.local>
2019-11-23 15:28:06 -0700

    Fixed load script

--- MiniProjectTwo/LoadFiles.sh
+++ MiniProjectTwo/LoadFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sort -u recs.txt -o recs.txt | cat recs.txt | perl ./break.pl |  db_load -T -t hash re.idx
-sort -u terms.txt -o terms.txt |cat terms.txt | perl ./break.pl | db_load -c duplicates=1 -T -t btree te.idx
-sort -u emails.txt -o emails.txt | cat emails.txt | perl ./break.pl | db_load -c duplicates=1 -T -t btree em.idx
-sort -u dates.txt -o dates.txt | cat dates.txt | perl ./break.pl | db_load -c duplicates=1 -T -t btree da.idx
+cat recs.txt | sort -u | perl break.pl |  db_load -T -t hash -c dupsort=1 re.idx
+cat terms.txt  | sort -u |  perl break.pl | db_load -T -t btree -c dupsort=1 te.idx
+cat emails.txt | sort -u | perl break.pl | db_load -T -t btree -c dupsort=1 em.idx
+cat dates.txt  | sort -u |  perl break.pl | db_load -T -t btree  -c dupsort=1 da.idx
