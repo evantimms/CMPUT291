1b46cdd00636695b486284735fad0051e3840d11
Evan Perret-Timms <evan.perret@ca-cal01-lt177.local>
2019-11-23 19:11:08 -0700

    Made datequery function

--- MiniProjectTwo/DBMS.py
+++ MiniProjectTwo/DBMS.py
@@ -1,4 +1,5 @@
 from bsddb3 import db
+from datetime import datetime
 
 class DBMS:
     def __init__(self):
@@ -15,17 +16,40 @@ class DBMS:
         self.emails_cursor = self.emails_DB.cursor()
         self.recs_cursor = self.recs_DB.cursor()
 
-        self.master_ids = set() # set which holds all of the ids
+        self.master_ids = None
+        self.query_count = 0
 
     def runDateQuery(self, date, operator):
         """
         Runs a date query and intersets the results with the master ids
         """
+        date = datetime.strptime(date, "%Y/%m/%d")
+        curr = self.dates_cursor.first()
+        res = set()
+        while curr:
+            curr_date = datetime.strptime(curr[0].decode(), "%Y/%m/%d")
+            row_id = int(curr[1].decode())
+            append = (operator == ":" and date == curr_date) \
+                or (operator == ">" and curr_date > date) \
+                or (operator == "<" and curr_date < date) \
+                or (operator == ">=" and curr_date >= date) \
+                or (operator == "<=" and curr_date <= date)
+            
+            if append : res.add(row_id)
 
-    def runEmailQuery(self, firstTerm, secondTerm):
+            curr = self.dates_cursor.next()
+        
+        self._addToMasterIds(res)
+
+
+    def runEmailQuery(self, emailPrefix, firstTerm, secondTerm):
         """
         Runs an email query and intersets the results with the master ids
         """
+        res = set()
+
+
+        self._addToMasterIds(res)
 
     def runTermQuery(self, prefix, term, suffix):
         """
@@ -44,3 +68,29 @@ class DBMS:
         Resets the master ids to prepare for a new query
         """
         self.master_ids = set()
+        self.query_count = 0
+
+    def _addToMasterIds(self, res):
+        """
+        Intersects a set of ids to the master copy held in mem.
+        """
+        if not self.query_count:
+            self.master_ids = res
+        else:
+            self.master_ids = self.master_ids.intersection(res)
+        print(self.master_ids)
+        self.query_count += 1
+
+
+# Tests
+# dbms = DBMS()
+# dbms.runDateQuery("2000/10/02", ":")
+# dbms.resetQuery()
+# dbms.runDateQuery("2000/10/02", ">")
+# dbms.resetQuery()
+# dbms.runDateQuery("2000/10/02", "<")
+# dbms.resetQuery()
+# dbms.runDateQuery("2000/10/02", ">=")
+# dbms.resetQuery()
+# dbms.runDateQuery("2000/10/02", "<=")
+# dbms.resetQuery()
